5faa676c0c3bdcc367eb203897a9951571533910
i1hwan <mtslzx@gmail.com>
2022-11-25 12:38:57 +0900

    modified:   findNews.py

--- findNews.py
+++ findNews.py
@@ -159,6 +159,15 @@ def findNews (scraping_news_count: int = 3) -> dict:
         {
             "outputs": [
                 {
+                    "basicCard": {
+                    "title": "지누랑!",
+                    "description": "공지사항을 가져왔어!",
+                    "thumbnail": {
+                        "imageUrl": "https://raw.githubusercontent.com/mtslzx/gnurang/main/img/sad.jpg",
+                        "width": 800,
+                        "height": 800
+                    }
+                    },
                     "carousel": {
                         "type": "listCard",
                         "items":
